e93d268f7663d7cbb81df443bbcae5a91aefc709
frank870622 <frank870622@gmail.com>
2018-05-20 17:36:27 +0800

    Add files via upload

--- project/graph.py
+++ project/graph.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as pl
-pl.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
-pl.rcParams['font.serif'] = ['Microsoft JhengHei']
 from matplotlib.gridspec import GridSpec
 import numpy
 from PIL import Image
+pl.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
+pl.rcParams['font.serif'] = ['Microsoft JhengHei']
 
 
 def fig2img(fig):
@@ -37,25 +37,116 @@ def fig2data(fig):
     buf = numpy.roll(buf, 3, axis=2)
     return buf
 
-figure = pl.figure()
-value = [33, 67]
-value2 = [40, 60]
-labels = '教師比例', '學生比例'
-colors = ['lightcoral', 'lightskyblue']
 
-thegrid = GridSpec(1, 2)
-pl.subplot(thegrid[0, 0], aspect=1)
-pl.pie(value, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
-pl.title('中葉大學')
+def drawing(string):
+    figure = pl.figure()
+    value = [33, 67]
+    value2 = [40, 60]
+    labels = '教師比例', '學生比例'
+    colors = ['lightcoral', 'lightskyblue']
 
-pl.subplot(thegrid[0, 1], aspect=1)
-pl.pie(value2, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
-pl.title('小葉大學')
+    thegrid = GridSpec(1, 2)
+    pl.subplot(thegrid[0, 0], aspect=1)
+    pl.pie(value, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
+    pl.title('中葉大學')
+
+    pl.subplot(thegrid[0, 1], aspect=1)
+    pl.pie(value2, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
+    pl.title('小葉大學')
+
+    im = fig2img(figure)
+    im.show()
+    pl.gcf().clear()
+
+'''
+NCKU
+師生比:
+    學生:	21,252人(2017年03月)
+    專任教師:	1,326人(2017年03月)
+    兼任教師數 :	684(2017年03月)
+    註冊率			
+                大學	碩士	碩專  
+            95學年度	95.64%	96.64%	98.38%
+            96學年度	95.32%	95.38%	93.65%
+            97學年度	97.24%	95.57%	93.08%
+            98學年度	95.98%	96.94%	99.39%
+            99學年度	96.01%	92.05%	95.23%
+            100學年度	95.53%	95.59%	88.42%
+            101學年度	95.72%	95.01%	88.00%
+            102學年度	96.49%	93.89%	88.63%
+            103學年度	95.05%	93.96%	87.98%
+            104學年度	95.22%	92.67%	88.64%
+            105學年度	95.31%	94.85%	93.11%
+            106學年度	95.35%	95.32%	94.40%
+    
+    目前未就業原因
+    103 畢業學制
+                        學士班  碩士班  博士班
+        進修中          74.76   11.38   0.00
+    服役中或等待服役中  11.13   50.14   40.00
+    準備考試            7.68    12.47   5.00
+    尋找工作中          3.93    18.70   30.00
+    其他                2.50    7.32    25.00
+    總和                100.00  100.00  100.00
+    
+    學校可用資金 1,696,974 (千元)
+                    
+    學測分數:
+    https://www.caac.ccu.edu.tw/cacportal/apply_his_report/106/106_sieve_standard/report/pict/004.png
+    指考分數    
+中國文學系	412.00
+外國語文學系	497.55
+歷史學系	379.15
+台灣文學系	504.80
+數學系	365.45
+物理學系	429.25
+化學系	428.35
+地球科學系	334.50
+光電科學與工程學系	364.50
+機械工程學系	356.30
+化學工程學系	360.70
+材料科學及工程學系	370.40
+資源工程學系	342.00
+土木工程學系	346.80
+水利及海洋工程學系	440.50
+工程科學系	356.20
+系統及船舶機電工程學系	347.80
+航空太空工程學系	355.50
+環境工程學系	345.70
+測量及空間資訊學系	336.60
+生物醫學工程學系	357.10
+工業與資訊管理學系	351.30
+交通管理科學系	339.40
+企業管理學系	238.00
+統計學系	276.55
+會計學系	572.50
+醫學系(自費)	442.90
+醫學系(公費)	423.40
+醫學檢驗生物技術學系	420.60
+護理學系	363.33
+職能治療學系	370.70
+物理治療學系	392.18
+藥學系	414.85
+政治學系	422.55
+經濟學系	443.30
+法律學系	399.20
+心理學系	418.05
+電機工程學系	385.50
+資訊工程學系	376.80
+建築學系	431.43
+都市計劃學系	433.00
+工業設計學系	428.05
+生命科學系	451.53
+生物科技與產業科學系	447.85
+大一全校不分系學士學位學程	382.00
 
 
-im = fig2img(figure)
-im.show()
-pl.gcf().clear()
+
+
+    
+ 
+'''
+
 '''
 labels = '中葉大學', '小葉大學'
 
